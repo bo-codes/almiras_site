237b923456a1f8bc06b08dacdb77045019415020
elias <99310710+bo-codes@users.noreply.github.com>
2023-03-14 00:24:56 -0700

    finished placeholder AboutPage

--- frontend/src/pages/AboutPage/AboutPage.js
+++ frontend/src/pages/AboutPage/AboutPage.js
@@ -1,9 +1,38 @@
-import "./AboutPage.css"
+import "./AboutPage.css";
 
 const AboutPage = () => {
+  const randomNumGen = () => {
+    return Math.floor(Math.random() * (6 + 1) + 1);
+  };
+  const generateText = (name, num) => {
+    let finalStr = "";
+
+    for (let i = 0; i < num; i++) {
+      let currRepeats = randomNumGen();
+      for (let j = 0; j < currRepeats; j++) {
+        finalStr += "blah ";
+      }
+      finalStr += `${name} `;
+    }
+    return finalStr;
+  };
+
+  const almiraText = generateText("almira", 36);
+
   return (
-    <div><div>About Page</div></div>
-  )
-}
+    <div id="about-page">
+      <div id="about-container">
+        <div id="about-img"></div>
+        <div id="about-descr">
+          {almiraText.slice(0, almiraText.length / 3)} <br />{" "}
+          {almiraText.slice(almiraText.length / 3, almiraText.length / 1.5)}{" "}
+          <br />{" "}
+          {almiraText.slice(almiraText.length / 1.5, almiraText.length / 0.75)}{" "}
+          <br /> {almiraText.slice(almiraText.length / 0.75)}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default AboutPage;
