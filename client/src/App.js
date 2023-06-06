a2e5f5c6b553bf50bf971ac0d22835d86b80e2e3
elias <99310710+bo-codes@users.noreply.github.com>
2023-06-05 23:03:54 -0700

    forget previous push. accidentally pushed ilaydas site

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,26 @@ import HomePage from "./pages/HomePage/HomePage";
 import ResumePage from "./pages/ResumePage/ResumePage";
 import ErrPage from "./pages/ErrPage/ErrPage";
 
+import img1 from "./imgs/almis_gallery/1.jpeg";
+import img2 from "./imgs/almis_gallery/2.jpeg";
+import img3 from "./imgs/almis_gallery/3.jpeg";
+import img4 from "./imgs/almis_gallery/4.jpeg";
+import img4_5 from "./imgs/almis_gallery/4.jpg";
+import img5 from "./imgs/almis_gallery/5.jpeg";
+import img6 from "./imgs/almis_gallery/6.jpeg";
+import img7 from "./imgs/almis_gallery/7.jpeg";
+import img8 from "./imgs/almis_gallery/8.jpeg";
+import img9 from "./imgs/almis_gallery/9.jpeg";
+import img10 from "./imgs/almis_gallery/10.jpeg";
+import img11 from "./imgs/almis_gallery/11.jpeg";
+import img12 from "./imgs/almis_gallery/12.jpeg";
+import img13 from "./imgs/almis_gallery/13.jpeg";
+import img14 from "./imgs/almis_gallery/14.jpeg";
+import img15 from "./imgs/almis_gallery/15.jpg";
+import img16 from "./imgs/almis_gallery/16.jpg"
+
+const galleryImages = {img1, img2, img3, img4, img4_5, img5, img6, img7,img8, img9, img10, img11, img12, img13, img14, img15, img16}
+
 const App = () => {
   return (
     <>
@@ -18,7 +38,7 @@ const App = () => {
         <Route path="/" exact={true} element={<HomePage />} />
         <Route path="/about" exact={true} element={<AboutPage />} />
         <Route path="/resume" exact={true} element={<ResumePage />} />
-        <Route path="/gallery" exact={true} element={<GalleryPage />} />
+        <Route path="/gallery" exact={true} element={<GalleryPage galleryImages={galleryImages}/>} />
         <Route path="/err" exact={true} element={<ErrPage />} />
         <Route path="*" element={<Navigate to="/err" replace />} />
       </Routes>
--- client/src/pages/GalleryPage/GalleryPage.js
+++ client/src/pages/GalleryPage/GalleryPage.js
@@ -2,93 +2,100 @@ import { useEffect, useState } from "react";
 import ImgPrev from "../../components/ImgPrev/ImgPrev";
 import { Modal } from "../../components/Modal/index.js";
 import EnlargedImg from "../../components/EnlargedImg/EnlargedImg"
-import img1 from "../../imgs/almis_gallery/1.jpeg";
-import img2 from "../../imgs/almis_gallery/2.jpeg";
-import img3 from "../../imgs/almis_gallery/3.jpeg";
-import img4 from "../../imgs/almis_gallery/4.jpeg";
-import img5 from "../../imgs/almis_gallery/5.jpeg";
-import img6 from "../../imgs/almis_gallery/6.jpeg";
-import img7 from "../../imgs/almis_gallery/7.jpeg";
-import img8 from "../../imgs/almis_gallery/8.jpeg";
-import img9 from "../../imgs/almis_gallery/9.jpeg";
-import img10 from "../../imgs/almis_gallery/10.jpeg";
-import img11 from "../../imgs/almis_gallery/11.jpeg";
-import img12 from "../../imgs/almis_gallery/12.jpeg";
-import img13 from "../../imgs/almis_gallery/13.jpeg";
-import img14 from "../../imgs/almis_gallery/14.jpeg";
-import img15 from "../../imgs/almis_gallery/15.jpg";
-import img16 from "../../imgs/almis_gallery/16.jpg";
 import "./GalleryPage.css";
 
-const posts1 = [
-  { title: "Sparkle's Nighttime Story", desc: "desc", img: img1 },
-  { title: "Gator Rescue", desc: "desc", img: img2 },
-  { title: "Sparkle and Didi", desc: "desc", img: img3 },
-  { title: "Sparkle Color Study", desc: "desc", img: img4 },
-  { title: "Gator Friends", desc: "desc", img: img5 },
-  { title: "Glorby and Wanda", desc: "desc", img: img6 },
-  { title: "Bun Bun Sees a Rock", desc: "desc", img: img7 },
-  { title: "Joshua Tree Trip", desc: "desc", img: img8 },
-  { title: "Garden of Hope", desc: "desc", img: img9 },
-  { title: "Dancers", desc: "desc", img: img10 },
-  { title: "San Francisco", desc: "desc", img: img11 },
-  { title: "Bad Intentions", desc: "desc", img: img12 },
-  { title: "Mouse Trap", desc: "desc", img: img13 },
-  { title: "Frogster", desc: "desc", img: img14 },
-  { title: "Gator Babe", desc: "desc", img: img15 },
-  { title: "Bobby, My Honey", desc: "desc", img: img16 },
-  //
-  //
-];
-
-const posts2 = [
-  { title: "title", desc: "desc", img: img1 },
-  { title: "title", desc: "desc", img: img2 },
-  { title: "title", desc: "desc", img: img3 },
-  { title: "title", desc: "desc", img: img4 },
-  { title: "title", desc: "desc", img: img5 },
-  { title: "title", desc: "desc", img: img6 },
-  { title: "title", desc: "desc", img: img7 },
-  { title: "title", desc: "desc", img: img8 },
-  { title: "title", desc: "desc", img: img9 },
-  { title: "title", desc: "desc", img: img10 },
-  { title: "title", desc: "desc", img: img11 },
-  { title: "title", desc: "desc", img: img12 },
-  { title: "title", desc: "desc", img: img13 },
-  { title: "title", desc: "desc", img: img14 },
-  //
-];
-
-const posts3 = [
-  { title: "title", desc: "desc", img: img1 },
-  { title: "title", desc: "desc", img: img2 },
-  { title: "title", desc: "desc", img: img3 },
-  { title: "title", desc: "desc", img: img4 },
-  { title: "title", desc: "desc", img: img5 },
-  { title: "title", desc: "desc", img: img6 },
-  { title: "title", desc: "desc", img: img7 },
-  { title: "title", desc: "desc", img: img8 },
-  { title: "title", desc: "desc", img: img9 },
-  { title: "title", desc: "desc", img: img10 },
-  { title: "title", desc: "desc", img: img11 },
-  { title: "title", desc: "desc", img: img12 },
-  { title: "title", desc: "desc", img: img13 },
-  { title: "title", desc: "desc", img: img14 },
-];
 
 
-const GalleryPage = () => {
-  useEffect(() => {
-    if (window.innerWidth < 1778 && window.innerWidth > 705) {
-      setPosts(posts2)
-    }
-    else if (window.innerWidth >= 1778) {
-      setPosts(posts1)
-    }
-    else if (window.innerWidth <= 705) {
-      setPosts(posts3)
-    }
-  }, [window.innerWidth])
+// const posts2 = [
+//   { title: "title", desc: "desc", img: img1 },
+//   { title: "title", desc: "desc", img: img2 },
+//   { title: "title", desc: "desc", img: img3 },
+//   { title: "title", desc: "desc", img: img4 },
+//   { title: "title", desc: "desc", img: img5 },
+//   { title: "title", desc: "desc", img: img6 },
+//   { title: "title", desc: "desc", img: img7 },
+//   { title: "title", desc: "desc", img: img8 },
+//   { title: "title", desc: "desc", img: img9 },
+//   { title: "title", desc: "desc", img: img10 },
+//   { title: "title", desc: "desc", img: img11 },
+//   { title: "title", desc: "desc", img: img12 },
+//   { title: "title", desc: "desc", img: img13 },
+//   { title: "title", desc: "desc", img: img14 },
+//   //
+// ];
+
+// const posts3 = [
+//   { title: "title", desc: "desc", img: img1 },
+//   { title: "title", desc: "desc", img: img2 },
+//   { title: "title", desc: "desc", img: img3 },
+//   { title: "title", desc: "desc", img: img4 },
+//   { title: "title", desc: "desc", img: img5 },
+//   { title: "title", desc: "desc", img: img6 },
+//   { title: "title", desc: "desc", img: img7 },
+//   { title: "title", desc: "desc", img: img8 },
+//   { title: "title", desc: "desc", img: img9 },
+//   { title: "title", desc: "desc", img: img10 },
+//   { title: "title", desc: "desc", img: img11 },
+//   { title: "title", desc: "desc", img: img12 },
+//   { title: "title", desc: "desc", img: img13 },
+//   { title: "title", desc: "desc", img: img14 },
+// ];
+
+
+const GalleryPage = ({galleryImages}) => {
+
+  const {
+    img1,
+    img2,
+    img3,
+    img4,
+    img4_5,
+    img5,
+    img6,
+    img7,
+    img8,
+    img9,
+    img10,
+    img11,
+    img12,
+    img13,
+    img14,
+    img15,
+    img16,
+  } = galleryImages;
+
+  const posts1 = [
+    { title: "Sparkle's Nighttime Story", desc: "desc", img: img1 },
+    { title: "Gator Rescue", desc: "desc", img: img2 },
+    { title: "Sparkle and Didi", desc: "desc", img: img3 },
+    { title: "Sparkle Color Study", desc: "desc", img: img4 },
+    { title: "Protectors of Nature", desc: "desc", img: img4_5 },
+    { title: "Gator Friends", desc: "desc", img: img5 },
+    { title: "Glorby and Wanda", desc: "desc", img: img6 },
+    { title: "Bun Bun Sees a Rock", desc: "desc", img: img7 },
+    { title: "Joshua Tree Trip", desc: "desc", img: img8 },
+    { title: "Garden of Hope", desc: "desc", img: img9 },
+    { title: "Dancers", desc: "desc", img: img10 },
+    { title: "San Francisco", desc: "desc", img: img11 },
+    { title: "Bad Intentions", desc: "desc", img: img12 },
+    { title: "Mouse Trap", desc: "desc", img: img13 },
+    { title: "Frogster", desc: "desc", img: img14 },
+    { title: "Gator Babe", desc: "desc", img: img15 },
+    { title: "Bobby, My Honey", desc: "desc", img: img16 },
+    //
+    //
+  ];
+  // useEffect(() => {
+  //   if (window.innerWidth < 1778 && window.innerWidth > 705) {
+  //     setPosts(posts2)
+  //   }
+  //   else if (window.innerWidth >= 1778) {
+  //     setPosts(posts1)
+  //   }
+  //   else if (window.innerWidth <= 705) {
+  //     setPosts(posts3)
+  //   }
+  // }, [window.innerWidth])
 
   const [showEnlargedImg, setShowEnlargedImg] = useState(false);
   const [currentPost, setCurrentPost] = useState("");
